f6a7d6253d6d4e1e32034826b6eb940e42071616
Ekrem402 <ekrem.ciftci@lojimax.com.tr>
2025-04-22 11:48:21 +0300

    Update lojimax_app.py

--- lojimax_app.py
+++ lojimax_app.py
@@ -9,18 +9,20 @@ st.title("LOJIMAX - ÖZET (Google Drive Üzerinden Excel)")
 url = "https://drive.google.com/uc?id=1iU-Q96InL-DPi3OcrjbG8XnU_mcx_Tv_"
 output = "lojimax_maliyet.xlsx"
 
-# Dosyayı indir (varsa tekrar indirme)
+# Dosyayı indir
 gdown.download(url, output, quiet=False)
 
-# Excel'in "ÖZET" sayfasını oku (başlık 2. satırda)
+# Excel dosyasının "ÖZET" sayfasını oku (başlık 2. satırda)
 df = pd.read_excel(output, sheet_name="ÖZET", header=1)
 
-# İlk 8 satırı ve 17–21. sütunlar arasını al
+# İlk 8 satırı al
 df_first8 = df.head(8)
-df_selected_columns = df_first8.iloc[:, 17:22]
+
+# İlk 9 sütun (0-8) ve 17-21 arası sütunları birleştir
+df_selected = pd.concat([df_first8.iloc[:, 0:9], df_first8.iloc[:, 17:22]], axis=1)
 
 # Alt başlık
 st.subheader("📌 ÖZET - Seçilen Sütunlar")
 
 # Tabloyu göster
-st.dataframe(df_selected_columns)
+st.dataframe(df_selected)
